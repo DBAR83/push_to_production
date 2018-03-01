0611dd574672ee75372dc663831be4c8d94c04ae
Dustin B <dbar83@hotmail.com>
2018-03-01 13:01:44 -0500

    added associations keys into models

--- db/migrate/20180301160119_create_teams.rb
+++ db/migrate/20180301160119_create_teams.rb
@@ -2,7 +2,7 @@ class CreateTeams < ActiveRecord::Migration[5.1]
   def change
     create_table :teams do |t|
       t.string :name
-      t.model :sport
+      t.string :sport
 
       t.timestamps
     end
